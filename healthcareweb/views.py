53363c59d22bc8844b175f54d001403529f09fc8
Sima Karim <Sima Karim>
2016-04-01 23:34:15 +0200

    add db connection

--- healthcareweb/views.py
+++ healthcareweb/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from healthcareweb.db.mysqlconn import getConnection
 
 def index(request):
+	conn = getConnection()
 	return render(request, 'index.html')
 
 def signup(request):
